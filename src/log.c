1dc58ea311b928ac3340e3c1bd386d2229ca93ee
Nicolas J. Bouliane <nicboul@gmail.com>
2020-05-08 23:49:08 +0000

    add log_debug and make log lvl optional

--- src/log.c
+++ src/log.c
@@ -1,15 +1,72 @@
 #include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 #include <time.h>
 
+#include "log.h"
+
 static void	(*cb_log)(const char *) = NULL;
+
+static uint8_t	log_lvl_debug;
+static uint8_t	log_lvl_info;
+static uint8_t	log_lvl_warn;
 
 void
 log_setcb(void (*cb)(const char *))
 {
 	cb_log = cb;
+}
+
+void
+log_enable(uint8_t log_lvl, uint8_t onoff)
+{
+	switch (log_lvl) {
+	case LOG_LVL_DEBUG:
+		log_lvl_debug = onoff;
+		break;
+	case LOG_LVL_INFO:
+		log_lvl_info = onoff;
+		break;
+	case LOG_LVL_WARN:
+		log_lvl_warn = onoff;
+		break;
+	}
+
+	return;
+}
+
+void
+log_debug(const char *format, ...)
+{
+	struct tm	*tm_info;
+	time_t		 timer;
+	va_list		 list;
+	int		 len;
+	static char	 buff[512];
+	char		 cur_time[20];
+
+	if (log_lvl_debug != 0)
+		return;
+
+	time(&timer);
+	tm_info = localtime(&timer);
+	strftime(cur_time, 20, "%Y-%m-%d %H:%M:%S", tm_info);
+
+	len = snprintf(buff, sizeof(buff), "[%s] debug> ", cur_time);
+
+	va_start(list, format);
+	len += vsnprintf(buff + len, sizeof(buff) - len, format, list);
+	snprintf(buff + len, sizeof(buff) - len, "\n");
+	va_end(list);
+
+	if (cb_log)
+		cb_log(buff);
+	else {
+		fprintf(stdout, "%s", buff);
+		fflush(stdout);
+	}
 }
 
 void
@@ -22,11 +79,14 @@ log_info(const char *format, ...)
 	static char	 buff[512];
 	char		 cur_time[20];
 
+	if (log_lvl_info == 0)
+		return;
+
 	time(&timer);
 	tm_info = localtime(&timer);
 	strftime(cur_time, 20, "%Y-%m-%d %H:%M:%S", tm_info);
 
-	len = snprintf(buff, sizeof(buff), "[%s] ", cur_time);
+	len = snprintf(buff, sizeof(buff), "[%s] info> ", cur_time);
 
 	va_start(list, format);
 	len += vsnprintf(buff + len, sizeof(buff) - len, format, list);
@@ -51,11 +111,14 @@ log_warnx(const char *format, ...)
 	static char	 buff[512];
 	char		 cur_time[20];
 
+	if (log_lvl_warn == 0)
+		return;
+
 	time(&timer);
 	tm_info = localtime(&timer);
 	strftime(cur_time, 20, "%Y-%m-%d %H:%M:%S", tm_info);
 
-	len = snprintf(buff, sizeof(buff), "[%s] ", cur_time);
+	len = snprintf(buff, sizeof(buff), "[%s] warn> ", cur_time);
 
 	va_start(list, format);
 	len += vsnprintf(buff + len, sizeof(buff) - len, format, list);
@@ -80,11 +143,14 @@ log_warn(const char *format, ...)
 	static char	 buff[512];
 	char		 cur_time[20];
 
+	if (log_lvl_warn == 0)
+		return;
+
 	time(&timer);
 	tm_info = localtime(&timer);
 	strftime(cur_time, 20, "%Y-%m-%d %H:%M:%S", tm_info);
 
-	len = snprintf(buff, sizeof(buff), "[%s] ", cur_time);
+	len = snprintf(buff, sizeof(buff), "[%s] warn> ", cur_time);
 
 	va_start(list, format);
 	len += vsnprintf(buff + len, sizeof(buff) - len, format, list);
@@ -97,5 +163,4 @@ log_warn(const char *format, ...)
 		fprintf(stderr, "%s", buff);
 		fflush(stderr);
 	}
-
 }
--- src/log.h
+++ src/log.h
@@ -1,7 +1,12 @@
 #ifndef LOG_H
 #define LOG_H
 
+#define LOG_LVL_DEBUG	0x1
+#define LOG_LVL_INFO	0x2
+#define LOG_LVL_WARN	0x3
+
 void	log_setcb(void (*cb)(const char *));
+void	log_debug(const char *format, ...);
 void	log_info(const char *format, ...);
 void	log_warnx(const char *format, ...);
 void	log_warn(const char *format, ...);
