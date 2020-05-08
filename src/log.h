41b637f6ba99a9c0e03b7d7176570e0787e20111
Nicolas J. Bouliane <nicboul@gmail.com>
2020-05-08 23:52:37 +0000

    export log_enable

--- src/log.h
+++ src/log.h
@@ -1,10 +1,13 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <stdint.h>
+
 #define LOG_LVL_DEBUG	0x1
 #define LOG_LVL_INFO	0x2
 #define LOG_LVL_WARN	0x3
 
+void	log_enable(uint8_t, uint8_t);
 void	log_setcb(void (*cb)(const char *));
 void	log_debug(const char *format, ...);
 void	log_info(const char *format, ...);
