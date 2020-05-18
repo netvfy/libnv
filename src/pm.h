4dda8070a6e59ab6987261f43d7a9ecc0b21b42b
Nicolas Bouliane <nbouliane@digitalocean.com>
2020-05-18 19:54:43 -0400

    add prometheus exporter

---
+++ src/pm.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020 Nicolas Bouliane <nicboul@gmail.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef PM_H
+#define PM_H
+
+typedef struct	pm_metric;
+
+pm_metric	*pm_metric_add(const char *, const char *, const char *, const int, ...);
+int		 pm_metric_bins(pm_metric *, const int, ...);
+int		 pm_counter_add(pm_metric *, const double, ...);
+int		 pm_gauge_set(pm_metric *, const double, ...);
+int		 pm_gauge_add(pm_metric *, const double, ...);
+int		 pm_histogram_observe(pm_metric *, const double, ...);
+size_t		 pm_dump(char *, const size_t);
+
+#endif
